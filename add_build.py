c76c8a723f308d78ddc129462af18605b3a63130
ramesh6859 <ramesh6859@gmail.com>
2023-12-12 19:35:19 +0530

    added add_build.py

---
+++ add_build.py
@@ -0,0 +1,10 @@
+import json
+
+with open('package.json', 'r+') as f:
+    data = json.load(f)
+    data['scripts']['build'] = "echo 'Build not configured'"
+    data['scripts']['test'] = "echo 'Test not configured'"
+    print(data['scripts'])
+    f.seek(0)
+    json.dump(data, f, indent=4)
+    f.truncate()
